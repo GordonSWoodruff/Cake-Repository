ba34fa687a5517dfda2f5fe026d02ea8e165c5dc
GordonSWoodruff <91490276+GordonSWoodruff@users.noreply.github.com>
2021-10-11 12:18:15 -0400

    Is she wearing a short skirt and long jacket?

---
+++ src/New/Test.java
@@ -0,0 +1,29 @@
+package New;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a program. Yeaaaaaah.");
+	}
+	
+	private static void shortSkirtLongJacket() {
+		String mind = "diamond";
+		String knows = "best";
+		boolean cuttingShoes = true;
+		double burningEyes = 752;
+		boolean allocations = true;
+		boolean fast = true;
+		boolean thorough = true;
+		String sharpness = "tack";
+		
+		if (cuttingShoes == true & burningEyes > 700) {
+			if (allocations == true && fast == true && thorough == true) {
+				boolean shortSkirtLongJacket = true;
+			}
+		}
+		
+		System.out.println(shortSkirtLongJacket);
+	}
+
+}
